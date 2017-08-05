2658d0eba797037fe8193c387e7250ad5014c548
zhaochuanxing <zhaochuanxing@gmail.com>
2017-08-05 23:17:31 +0800

    add cardview

--- app/src/main/java/com/cxing/mygank/activity/HomeActivity.java
+++ app/src/main/java/com/cxing/mygank/activity/HomeActivity.java
@@ -1,18 +1,23 @@
 package com.cxing.mygank.activity;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.cxing.mygank.R;
 
@@ -49,6 +54,23 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             }
         });
+        FloatingActionButton floatingActionButton = (FloatingActionButton)findViewById(R.id.floating_btn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Snackbar.make(v,"data deleted",Snackbar.LENGTH_LONG).setAction("撤销",new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "floating", Toast.LENGTH_SHORT).show();
+
+                    }
+                }).show();
+            }
+        });
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview);
+
     }
 
     @Override
