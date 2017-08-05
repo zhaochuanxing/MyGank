52b8c624c49651e1802f1deabafdd12ad7864dfc
zhaochuanxing <zhaochuanxing@gmail.com>
2017-08-05 22:54:34 +0800

    add navigation view

--- app/src/main/java/com/cxing/mygank/activity/HomeActivity.java
+++ app/src/main/java/com/cxing/mygank/activity/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.cxing.mygank.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.cxing.mygank.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = HomeActivity.class.getSimpleName();
     private Toolbar mToolBar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +30,41 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        this.mToolBar = (Toolbar)findViewById(R.id.toolbar);
+        this.mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        this.mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerlayout);
+//        mDrawerLayout.closeDrawer(GravityCompat.START);
         setSupportActionBar(mToolBar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher);
+        }
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navi_view);
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.i(TAG,"onNavigationItemSelected"+item);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "onOptionsItemSelected" + item);
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
